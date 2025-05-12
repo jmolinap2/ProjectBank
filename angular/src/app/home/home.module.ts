88d664b17fae588141fc220cd8c206c52e62bb73
Jesus M <jmolinap2@unemi.edu.ec>
2025-05-11 22:23:24 -0500

    reorganizacion de carpetas

--- angular/src/app/home/home.module.ts
+++ angular/src/app/home/home.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '@shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { CommonModule } from '@angular/common';
+import { ChartModule } from 'primeng/chart';
+
+import { NgChartsModule } from 'ng2-charts';
 
 @NgModule({
     declarations: [HomeComponent],
-    imports: [SharedModule, HomeRoutingModule],
+    imports: [SharedModule,NgChartsModule, HomeRoutingModule, CommonModule,ChartModule],
 })
 export class HomeModule {}
